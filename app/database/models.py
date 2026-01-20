49a89c3db8074fd2f95e9b24be1169e39ff561a4
Qu5antum <erae88959@gmail.com>
2026-01-20 22:28:43 +0300

    series model

--- app/database/models.py
+++ app/database/models.py
@@ -7,7 +7,14 @@ actor_movies = Table(
     "actor_movies",
     Base.metadata,
     Column("actor_id", ForeignKey("actors.id"), primary_key=True),
-    Column("movie_id", ForeignKey("movies.id"), primary_key=True),
+    Column("movie_id", ForeignKey("movies.id"), primary_key=True)
+)
+
+actor_series = Table(
+    "actor_series",
+    Base.metadata,
+    Column("actor_id", ForeignKey("actors.id"), primary_key=True),
+    Column("series_id", ForeignKey("serieses.id"), primary_key=True)
 )
 
 class Movie(Base):
@@ -18,10 +25,27 @@ class Movie(Base):
     year: Mapped[int] = mapped_column(Integer)
     description: Mapped[str] = mapped_column(String)
     watched: Mapped[bool] = mapped_column(Boolean, default=False)
-    actors: Mapped[list["Actor"]] = relationship(
+
+    movie_actors: Mapped[list["Actor"]] = relationship(
         secondary=actor_movies,
         back_populates="movies"
     )
+
+
+class Series(Base):
+    __tablename__ = "serieses"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String)
+    year: Mapped[int] = mapped_column(Integer)
+    description: Mapped[str] = mapped_column(String)
+    watched: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    series_actors: Mapped[list["Actor"]] = relationship(
+        secondary=actor_series,
+        back_populates="serieses"
+    )
+
 
 
 class Actor(Base):
@@ -29,10 +53,19 @@ class Actor(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String, unique=True)
+
     movies: Mapped[list["Movie"]] = relationship(
         secondary=actor_movies,
-        back_populates="actors"
+        back_populates="movie_actors"
     )
+
+    serieses: Mapped[list["Series"]] = relationship(
+        secondary=actor_movies,
+        back_populates="series_actors"
+    )
+
+
+
     
 
 
