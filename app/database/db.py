529382ca0b66903e7b15dcb2fa2c9aaf41f1e676
Qu5antum <erae88959@gmail.com>
2026-01-21 21:05:46 +0300

    update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -3,12 +3,25 @@ from sqlalchemy import create_engine
 from app.database.base import Base
 from app.database import models
 from pathlib import Path
+import sys
+import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+def get_base_dir():
+    if getattr(sys, "frozen", False):
+        return Path(sys.executable).parent
+    else:
+        return Path(__file__).resolve().parent.parent
+
+
+BASE_DIR = get_base_dir()
 DB_PATH = BASE_DIR / "movies.db"
 
-engine = create_engine(f"sqlite:///{DB_PATH}")
+engine = create_engine(
+    f"sqlite:///{DB_PATH}",
+    connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(bind=engine)
 
 Base.metadata.create_all(bind=engine)
-
